b886ae68b2590c0d02d3a1356242790080ebaa61
Fabio Cipriani <FabioCipriani@mbp-di-fabio.station>
2016-08-25 08:13:15 +0200

    correctly implemented a segue from the selection of a pokemon to a new view controller, where we are going to show the selected pokemon's details.

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Fabio Cipriani on 25/08/16.
+//  Copyright © 2016 Fabio Cipriani. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -109,6 +109,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        var poke: Pokemon!
+        
+        if inSearchMode {
+            poke = filteredPokemon[indexPath.row]
+        } else {
+            poke = pokemons[indexPath.row]
+        }
+        
+        performSegue(withIdentifier: "PokemonDetailVC", sender: poke)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -126,6 +135,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         return CGSize(width: 105, height: 105)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PokemonDetailVC" {
+            if let detailsVC = segue.destination as? PokemonDetailVC {
+                if let poke = sender as? Pokemon {
+                    detailsVC.pokemon = poke
+                }
+            }
+        }
     }
     
     @IBAction func musicBtnPressed(_ sender: UIButton) {
