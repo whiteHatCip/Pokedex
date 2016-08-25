1c9618657653f60bb68bbafeecbdaa36eba3b519
Fabio Cipriani <FabioCipriani@mbp-di-fabio.station>
2016-08-25 07:06:54 +0200

    added a button to the ui to stop the music playing, added a music.mp3 file for a background sound that keeps looping. Added the csv.swift file that provides a function to parse a csv file to obtain a Dictionary from it. Added a pokemon.ttf font used in the header of the UI. In the view controller protocols for managing the collectionView were implemented, plus the music button handler to stop the music from playing; a parsePokemon csv file has been implemented to create an array of pokemon starting from a csv file then parsed into a Dictionary. Lastly, the searchBar is to be setup so that when text is there inserted, the app will filter out automatically the pokemons based on the inserted text

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Fabio Cipriani on 25/08/16.
+//  Copyright © 2016 Fabio Cipriani. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,13 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
+    @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var musicButton: UIButton!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var pokemons = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
+    var inSearchMode = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collection.delegate = self
+        collection.dataSource = self
+        searchBar.delegate = self
+        
+        parsePokemonCSV()
+        initAudio()
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+        } else {
+            inSearchMode = true
+            
+            let lower = searchBar.text!.lowercased()
+            
+        }
+    }
+    
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")
+        
+        do {
+            let csv = try CSV(contentsOfURL: path!)
+            let rows = csv.rows
+            
+            for row in rows {
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                let pokemon = Pokemon(nome: name, ID: pokeID)
+                pokemons.append(pokemon)
+            }
+            print(pokemons.count)
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = pokemons[indexPath.row]
+            cell.configureCell(pokemon)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return pokemons.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 105, height: 105)
+    }
+    
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
 }
 
