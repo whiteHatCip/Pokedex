1acea7dbe1555f90680f9dc54961fead148dd0c7
Fabio Cipriani <FabioCipriani@mbp-di-fabio.station>
2016-08-26 04:47:18 +0200

    added constants.swift file

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Fabio Cipriani on 26/08/16.
+//  Copyright © 2016 Fabio Cipriani. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "/api/v1/pokemon/"
+
